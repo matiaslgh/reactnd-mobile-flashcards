041237d6950c6bdf7a7435fd64a6068bda93dda8
matiaslgh <matiaslgh@gmail.com>
2017-12-27 16:13:48 -0300

    fix: fetchAllDecks was not working

--- utils/api.js
+++ utils/api.js
@@ -1,16 +1,17 @@
 import { AsyncStorage } from 'react-native'
 
 export const fetchAllDecks = () =>
-  AsyncStorage.getAllKeys()
-    .then(decks => {
-      const allDecks = JSON.parse(decks)
-      return !allDecks ?
-        [] :
-        Object.keys(allDecks).map(key => ({
-          title: allDecks[key].title,
-          cardsCount: allDecks[key].questions.length
-        }))
-    })
+  AsyncStorage.getAllKeys().then( keys =>
+    AsyncStorage.multiGet(keys).then( stores =>
+      stores.map( mapDeck => {
+        const deck = JSON.parse(mapDeck[1])
+        return {
+          title: deck.title,
+          cardsCount: deck.questions.length
+        }
+      })
+    )
+  )
 
 export const addDeck = deck =>
   AsyncStorage.setItem(deck.title, JSON.stringify(deck))
